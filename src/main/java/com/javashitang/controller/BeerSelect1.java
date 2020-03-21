a1956fd3b0d794d2965d5fe0e374325cd1706d5e
lilimin <290059742@qq.com>
2020-03-21 15:33:24 +0800

    1.不用xml来学习servlet

--- src/main/java/com/javashitang/controller/BeerSelect1.java
+++ src/main/java/com/javashitang/controller/BeerSelect1.java
@@ -1,35 +1,26 @@
 package com.javashitang.controller;
 
-import com.javashitang.model.BeerExpert;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.List;
 
+@WebServlet(urlPatterns="/select")
 public class BeerSelect1 extends HttpServlet {
 
     /*
      * servlet的基本使用
      */
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         //使用doPost来处理Http请求，因为Html表单指出method = POST
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("Beer Selection Advice<br>");
-        String colorValue = req.getParameter("color");
-        BeerExpert be = new BeerExpert();
-        List result = be.getBrands(colorValue);
-        Iterator it = result.iterator();
-        while (it.hasNext()) {
-            out.print("<br>try: " + it.next());
-        }
+        out.println("hello");
     }
 }
