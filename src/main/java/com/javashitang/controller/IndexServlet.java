de0355e493788b49eefeb8291ba4b97d090ffdbf
lilimin <290059742@qq.com>
2021-04-03 20:17:20 +0800

    增加demo

--- src/main/java/com/javashitang/controller/IndexServlet.java
+++ src/main/java/com/javashitang/controller/IndexServlet.java
@@ -1,7 +1,9 @@
 package com.javashitang.controller;
 
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,18 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns="/select")
-public class BeerSelect1 extends HttpServlet {
+@WebServlet(urlPatterns="/index", initParams = {
+        @WebInitParam(name = "library.name", value = "javashitang")
+})
+public class IndexServlet extends HttpServlet {
 
-    /*
-     * servlet的基本使用
-     */
+    private String libraryName;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.libraryName = config.getInitParameter("library.name");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("step into doGet");
-        //使用doPost来处理Http请求，因为Html表单指出method = POST
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
-        out.println("hello");
+        out.println("welcome to " + libraryName + " library");
     }
 }
--- src/main/java/com/javashitang/controller/UserServlet.java
+++ src/main/java/com/javashitang/controller/UserServlet.java
@@ -1,27 +1,23 @@
 package com.javashitang.controller;
 
-import com.javashitang.model.Dog;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class ListenerTester extends HttpServlet {
-
-    /*
-     * 监听器的使用
-     */
+@WebServlet(urlPatterns="/user")
+public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        String userId = req.getParameter("userId");
+        String token = req.getHeader("loginToken");
+        resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
-
-        out.println("test context attributes set by listener<br>");
-        Dog dog = (Dog)getServletContext().getAttribute("dog");
-        out.println("Dog's breed is: " + dog.getBreed());
+        out.println("userId " + userId + " token " + token);
     }
 }
